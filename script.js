6260aa8570586ff8008107c85ce6dab78ef1575a
Prem Mhatre <83350182+Prem-Mhatre@users.noreply.github.com>
2021-11-05 18:56:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+butter = "";
+dynamite = "";
 function preload(){
-
+    dynamite = loadSound("dynamite.mp3");
+    butter = loadSound("butter.mp3");
 }
 
 function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
 
+    video = createCapture(VIDEO);
+    video.hide();
 }
 
 function draw(){
-    
+    image(video, 0, 0, 600, 500);
 }
