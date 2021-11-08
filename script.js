e620119986eb5b75d1be565a4caf0ddff3cd166f
Prem Mhatre <83350182+Prem-Mhatre@users.noreply.github.com>
2021-11-08 18:31:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 butter = "";
 dynamite = "";
+leftWristX = 0;
+leftWristY = 0; 
+rightWristX = 0;
+rightWristY = 0;
 function preload(){
     dynamite = loadSound("dynamite.mp3");
     butter = loadSound("butter.mp3");
@@ -11,8 +15,29 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw(){
     image(video, 0, 0, 600, 500);
+}
+
+function modelLoaded(){
+    console.log("pose net model is loaded");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+
+        console.log("LeftWristX: "+leftWristX+"" + "LeftWristY: "+leftWristY+"", "RightWristX: "+rightWristX+"", "RightWristY: "+rightWristY+"");
+    }
 }
